5b002a68299fb2d8a89ada2115bee5e1f4df38ee
Miftah8 <146458421+mifthrizky@users.noreply.github.com>
2025-07-05 10:41:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+//Menambah data di display
 function appendValue(value){
     document.getElementById('display').value += value;
 }
 
+//Clear data di display
 function clearDisplay(){
     document.getElementById('display').value = "";
 }
 
+//Menghitung value data
 function calculate(){
     const display = document.getElementById('display')
     try {
